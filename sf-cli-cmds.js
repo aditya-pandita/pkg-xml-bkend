ee97b3c11f68b7345420be0a6cfc272ff588bdb5
Rithik <rithik.pandita.143@gmail.com>
2024-04-19 19:27:07 +0530

    Added comments, handled catch errors

--- sf-cli-cmds.js
+++ sf-cli-cmds.js
@@ -14,81 +14,96 @@ let instance_url = '';
 
 async function invokeCmdSequence(auth_token, instance_url) {
 
-    const os = require('os');
+    try {
+        const os = require('os');
 
-    const platform = os.platform();
-    let envVariableInitCmd = 'export';
-    let envVariableRevokeCmd = 'unset SF_ACCESS_TOKEN';
-    let envDelCmd = 'del';
+        const platform = os.platform();
+        let envVariableInitCmd = 'export';
+        let envVariableRevokeCmd = 'unset SF_ACCESS_TOKEN';
+        let envDelCmd = 'del';
 
-    if (platform === 'darwin') {/** Mac OS */
+        if (platform === 'darwin') {/** Mac OS */
 
-        envVariableInitCmd = 'export';
-        envVariableRevokeCmd = 'unset SF_ACCESS_TOKEN=';
-        envDelCmd = 'rm';
-    }
-    else if (platform === 'win32') {/** Windows */
+            envVariableInitCmd = 'export';
+            envVariableRevokeCmd = 'unset SF_ACCESS_TOKEN=';
+            envDelCmd = 'rm';
+        }
+        else if (platform === 'win32') {/** Windows */
 
-        envVariableInitCmd = 'set';
-        envVariableRevokeCmd = 'set SF_ACCESS_TOKEN=';
-        envDelCmd = 'del';
-    }
-    else if (platform === 'linux') {/** Linux */
+            envVariableInitCmd = 'set';
+            envVariableRevokeCmd = 'set SF_ACCESS_TOKEN=';
+            envDelCmd = 'del';
+        }
+        else if (platform === 'linux') {/** Linux */
 
-        envVariableInitCmd = 'export';
-        envVariableRevokeCmd = 'unset SF_ACCESS_TOKEN';
-        envDelCmd = 'rm';
-    }
-    else {
-        console.log('Unknown OS');
-    }
-
-    if(auth_token && instance_url) {
-        execute('echo $SF_ACCESS_TOKEN');
-        // genShell(envVariableInitCmd, auth_token);
-        console.log('** process.env.SF_ACCESS_TOKEN  ->', process.env.SF_ACCESS_TOKEN);
-        process.env.SF_ACCESS_TOKEN = auth_token;
-        console.log('** process.env.SF_ACCESS_TOKEN  ->', process.env.SF_ACCESS_TOKEN);
-
-        /**
-         * 0. Delete Auth token as env variable
-         */
-        const deleteEnvAuthToken = envVariableRevokeCmd;
-        execute(deleteEnvAuthToken);
-        execute('echo $SF_ACCESS_TOKEN');
-
-        /**
-         * 1. Set Auth token as env variable
-         */
-        const setEnvAuthToken = envVariableInitCmd + ' SF_ACCESS_TOKEN=' + '\'' + auth_token  + '\'';
-        execute(setEnvAuthToken);
-
-        execute('echo $SF_ACCESS_TOKEN');
-
-        /**
-         * 2. Authorize and make an alias for the auth. org
-         */
-        const setAlias = 'sf org login access-token -r ' + instance_url + ' -a orgAlias';
-        await execute(setAlias);
-
-        /**
-         * 3. Run the cmd to 1st delete any existing "package.xml" files under "sf-project" folder
-         */
-        const filePath = './sf-project/package.xml';
-        // Check if the file exists
-        if (fs.existsSync(filePath)) {
-            const deletePkgXMlFile = envDelCmd + ' package.xml';
-            await execute(`cd ${'./sf-project'} && ${deletePkgXMlFile}`);
+            envVariableInitCmd = 'export';
+            envVariableRevokeCmd = 'unset SF_ACCESS_TOKEN';
+            envDelCmd = 'rm';
+        }
+        else {
+            console.log('Unknown OS');
         }
 
-        /**
-         * 4. Run the cmd to generate the orgs whle package.xml fiel with the help of "sf" npm
-         */
-        const genPkgXml = 'sf project generate manifest --from-org orgAlias';
-        await execute(`cd ${'./sf-project'} && ${genPkgXml}`);
+        if(auth_token && instance_url) {
+            execute('echo $SF_ACCESS_TOKEN');
+            // genShell(envVariableInitCmd, auth_token);
+            console.log('** process.env.SF_ACCESS_TOKEN  ->', process.env.SF_ACCESS_TOKEN);
+            process.env.SF_ACCESS_TOKEN = auth_token;
+            console.log('** process.env.SF_ACCESS_TOKEN  ->', process.env.SF_ACCESS_TOKEN);
 
-        console.log(`Finished execution`);
+            /**
+             * 0. Delete Auth token as env variable
+             */
+            console.log(`** Start: Delete existing Access Token`);
+            const deleteEnvAuthToken = envVariableRevokeCmd;
+            execute(deleteEnvAuthToken);
+            execute('echo $SF_ACCESS_TOKEN');
+            console.log(`** Finish: Delete existing Access Token`);
 
+            /**
+             * 1. Set Auth token as env variable
+             */
+            console.log(`** Start: Setting Auth Token as env variable`);
+            const setEnvAuthToken = envVariableInitCmd + ' SF_ACCESS_TOKEN=' + '\'' + auth_token  + '\'';
+            execute(setEnvAuthToken);
+            execute('echo $SF_ACCESS_TOKEN');
+            console.log(`** Finish: Setting Auth Token as env variable`);
+
+            /**
+             * 2. Authorize and make an alias for the auth. org
+             */
+            console.log(`** Start: Set Alias for the Org`);
+            const setAlias = 'sf org login access-token -r ' + instance_url + ' -a orgAlias';
+            await execute(setAlias);
+            console.log(`** Finish: Set Alias for the Org`);
+
+            /**
+             * 3. Run the cmd to 1st delete any existing "package.xml" files under "sf-project" folder
+             */
+            console.log(`** Start: Delete existing Package.xml file`);
+            const filePath = './sf-project/package.xml';
+            // Check if the file exists
+            if (fs.existsSync(filePath)) {
+                const deletePkgXMlFile = envDelCmd + ' package.xml';
+                await execute(`cd ${'./sf-project'} && ${deletePkgXMlFile}`);
+            }
+            console.log(`** Finish: Delete existing Package.xml file`);
+
+            /**
+             * 4. Run the cmd to generate the orgs whle package.xml fiel with the help of "sf" npm
+             */
+            console.log(`** Start: Retrieving Package.xml`);
+            const genPkgXml = 'sf project generate manifest --from-org orgAlias';
+            await execute(`cd ${'./sf-project'} && ${genPkgXml}`);
+            console.log(`** Finish: Retrieving Package.xml file`);
+
+            console.log(`Finished execution`);
+
+        }
+    }
+    catch(error) {
+
+        console.error('** Error occurred:', error);
     }
 }
 
